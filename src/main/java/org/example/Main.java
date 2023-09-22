3a7d913df508ac5b3c236c3b84f03b6c9761f62b
Dmytro Hurskyi <dmytro.hurskyi@boosta.co>
2023-09-22 11:25:06 +0300

    ltt

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class Main {
+    public static WebDriver init(String browser, WebDriver wd) {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--window-position=0,0");
+        options.addArguments("--host-resolver-rules=MAP js.intercomcdn.com 127.0.0.1");
+        String userAgentParam = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36";
+        options.addArguments("user-agent=" + userAgentParam);
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--chrome-frame");
+
+        wd = new ChromeDriver(options);
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wd.manage().timeouts().scriptTimeout(Duration.ofSeconds(90));
+        return wd;
+    }
+
+
+}
---
+++ src/main/java/org/example/lttCheck.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class lttCheck {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @Test
+    public void testLtt() {
+        driver = Main.init("chrome", driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));;
+        driver.get("https://ltt.rocks/login");
+        visibilityOfElementLocated("//input [@id = 'username']").sendKeys("adminuser");
+        visibilityOfElementLocated("//input [@id = 'password']").sendKeys("pass");
+        visibilityOfElementLocated("//input [@type = 'submit']").click();
+
+        Assert.assertTrue(visibilityOfElementLocated("//div [@id = 'linkList_wrapper']").isDisplayed());
+
+        visibilityOfElementLocated("//button [contains (@class,  'user-area__menu-button')]").click();
+        visibilityOfElementLocated("//a [@href = '/logout']").click();
+
+        Assert.assertTrue(visibilityOfElementLocated("//input [@id = 'username']").isDisplayed());
+
+        driver.manage().deleteAllCookies();
+        driver.navigate().refresh();
+    }
+
+    private WebElement visibilityOfElementLocated(String locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+    }
+}
