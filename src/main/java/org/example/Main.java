2ca0ba638a50f33f752addc8bd751808bcd051d5
Dmytro Hurskyi <dmytro.hurskyi@boosta.co>
2023-10-09 18:33:24 +0300

    ltt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,16 @@ import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 public class Main {
-    public static WebDriver init(WebDriver wd) {
+    public static WebDriver init( WebDriver wd) throws MalformedURLException {
 
-        WebDriverManager.chromedriver().setup();
+//        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("--window-size=1920,1080");
@@ -23,8 +26,9 @@ public class Main {
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--chrome-frame");
+//        options.addArguments("--headless");
 
-        wd = new ChromeDriver(options);
+        wd = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"),options, false);
         wd.manage().window().maximize();
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         wd.manage().timeouts().scriptTimeout(Duration.ofSeconds(90));
--- src/main/java/org/example/lttCheck.java
+++ src/main/java/org/example/lttCheck.java
@@ -6,8 +6,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
 import java.time.Duration;
 
 public class lttCheck {
@@ -15,25 +17,34 @@ public class lttCheck {
     WebDriver driver;
     WebDriverWait wait;
 
-    @Test
-    public void testLtt() {
+    @BeforeClass
+    public void driverInit() throws MalformedURLException{
         driver = Main.init(driver);
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
 
-        driver.get("https://ltt.rocks/login");
-        visibilityOfElementLocated("//input [@id = 'username']").sendKeys("adminuser");
-        visibilityOfElementLocated("//input [@id = 'password']").sendKeys("pass");
-        visibilityOfElementLocated("//input [@type = 'submit']").click();
+    @Test
+    public void testLtt()  {
+        while (true) {
+            try {
+                driver.get("https://ltt.rocks/login");
+                visibilityOfElementLocated("//input [@id = 'username']").sendKeys("adminuser");
+                visibilityOfElementLocated("//input [@id = 'password']").sendKeys("pass");
+                visibilityOfElementLocated("//input [@type = 'submit']").click();
 
-        Assert.assertTrue(visibilityOfElementLocated("//div [@id = 'linkList_wrapper']").isDisplayed());
+                Assert.assertTrue(visibilityOfElementLocated("//div [@id = 'linkList_wrapper']").isDisplayed());
 
-        visibilityOfElementLocated("//button [contains (@class,  'user-area__menu-button')]").click();
-        visibilityOfElementLocated("//a [@href = '/logout']").click();
+                visibilityOfElementLocated("//button [contains (@class,  'user-area__menu-button')]").click();
+                visibilityOfElementLocated("//a [@href = '/logout']").click();
 
-        Assert.assertTrue(visibilityOfElementLocated("//input [@id = 'username']").isDisplayed());
+                Assert.assertTrue(visibilityOfElementLocated("//input [@id = 'username']").isDisplayed());
 
-        driver.manage().deleteAllCookies();
-        driver.navigate().refresh();
+                driver.manage().deleteAllCookies();
+                driver.navigate().refresh();
+            } catch (Exception ex) {
+                String s = "";
+            }
+        }
     }
 
     private WebElement visibilityOfElementLocated(String locator) {
